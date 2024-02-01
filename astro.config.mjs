d20de9add2a8295361523f5b7bc77b02af4cb378
Jared M <j.mantell@wustl.edu>
2024-02-01 11:00:50 -0600

    a

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://hackathonsatwashu.github.io/",
+  site: "https://jaredmantell.github.io/",
   integrations: [tailwind(), icon()],
 });
